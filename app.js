6a8de055966369c1e2c557c13dc60c60c3c966ce
vigneswaranm <vigneswaran.m@freshdesk.com>
2017-03-16 16:24:38 +0530

    syntax issue no ES 6 support

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http')
-const port = 80
+const port = process.argv[2]
 
-const requestHandler = (request, response) => {
+const requestHandler = function(request, response)  {
   console.log(request.url)
 var a = '<html><head><title>?~U?~Jம?~Mப?~A வ?~Z?~M?~Z ?~Zி?~Y?~M?~Uம?~M?~_ா</title></head><body><p style="height: 100px; font-size: 50px;margin: 250px;padding: 10px 167px;font-size: 50;font-weight: bold;">?~U?~Jம?~Mப?~A வ?~Z?~M?~Z ?~Zி?~Y?~M?~Uம?~M?~_ா</p></body></html>';
   response.end(a);
@@ -9,7 +9,7 @@ var a = '<html><head><title>?~U?~Jம?~Mப?~A வ?~Z?~M?~Z ?~Zி?~Y?~M?~Uம?~
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
+server.listen(port, function(err) {
   if (err) {
     return console.log('something bad happened', err)
   }
