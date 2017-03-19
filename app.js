350bc187c1c51f0561497cdb460e877bb29c3df5
vigneswaranm <vigneswaran.m@freshdesk.com>
2017-03-19 23:04:09 +0530

    fix for syntax error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.argv[2]
 
 const requestHandler = function(request, response)  {
   console.log(request.url)
-var a = '<html><head><title>?~U?~Jம?~Mப?~A வ?~Z?~M?~Z ?~Zி?~Y?~M?~Uம?~M?~_ா</title></head><body><p style="height: 100px; font-size: 50px;margin: 250px;padding: 10px 167px;font-size: 50;font-weight: bold;">?~U?~Jம?~Mப?~A வ?~Z?~M?~Z ?~Zி?~Y?~M?~Uம?~M?~_ா</p></body></html>';
+var a = '<html><head><title>\‘கொம்பு வெச்ச சிங்கம்டா\’</title></head><body><p style="height: 100px; font-size: 50px;margin: 250px;padding: 10px 167px;font-size: 50;font-weight: bold;">\‘கொம்பு வெச்ச சிங்கம்டா\’</p></body></html>';
   response.end(a);
 }
 
@@ -14,5 +14,5 @@ server.listen(port, function(err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log('server is listening on ',port)
 })
